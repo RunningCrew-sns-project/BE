42a568ce2c463e9b816cf7bc668c389a244befb7
TaeYoung1217 <wkdxodud44@gmail.com>
2024-10-13 14:36:56 +0900

    feat : 블로그 기능 및 좋아요 기능 세팅

---
+++ src/main/java/com/github/accountmanagementproject/repository/userLikesBlog/UserLikesBlog.java
@@ -0,0 +1,29 @@
+package com.github.accountmanagementproject.repository.userLikesBlog;
+
+import com.github.accountmanagementproject.repository.account.users.MyUser;
+import com.github.accountmanagementproject.repository.blog.Blog;
+import jakarta.persistence.*;
+import lombok.*;
+import org.checkerframework.checker.units.qual.C;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "userLikesBlog")
+public class UserLikesBlog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_likes_blog_id")
+    private Integer userLikesBlogId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "blog_id")
+    private Blog blog;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private MyUser user;
+}
