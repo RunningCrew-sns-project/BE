a958be3d68c9423e39cb52691d4755ceb9057d64
yubin <yubincho9@gmail.com>
2024-11-02 20:11:09 +0900

    [RunJoinPost] - crewRepository 에 메서드 추가

--- src/main/java/com/github/accountmanagementproject/repository/crew/crew/CrewsRepository.java
+++ src/main/java/com/github/accountmanagementproject/repository/crew/crew/CrewsRepository.java
@@ -5,4 +5,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CrewsRepository extends JpaRepository<Crew, Long> {
+
+    Crew findByCrewMasterUserId(Long userId);
 }
