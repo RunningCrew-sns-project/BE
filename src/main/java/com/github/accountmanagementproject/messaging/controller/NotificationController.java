d4836acf72f0129c9629a38e9fc31771e4bf835b
yubin <yubincho9@gmail.com>
2024-11-05 17:12:05 +0900

    1. [RunJoinPost] - 예외처리 응답 수정 2. 알림 추가 - 카프카, sse

---
+++ src/main/java/com/github/accountmanagementproject/messaging/controller/NotificationController.java
@@ -0,0 +1,35 @@
+package com.github.accountmanagementproject.messaging.controller;
+
+import com.github.accountmanagementproject.messaging.repository.Notification;
+import com.github.accountmanagementproject.messaging.service.NotificationService;
+import com.github.accountmanagementproject.messaging.sse.SseEmitters;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+
+@RestController
+@RequestMapping("/api/notifications")
+public class NotificationController {
+
+    private final NotificationService notificationService;
+    private final SseEmitters sseEmitters;
+
+    public NotificationController(NotificationService notificationService, SseEmitters sseEmitters) {
+        this.notificationService = notificationService;
+        this.sseEmitters = sseEmitters;
+    }
+
+
+    @GetMapping("/subscribe")
+    public SseEmitter subscribe(@RequestParam Long userId) {
+        return sseEmitters.addEmitter(userId);
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendNotification(@RequestParam Long userId, @RequestBody String message) {
+        notificationService.notifyUser(userId, message);
+        return ResponseEntity.ok("Notification sent successfully!");
+    }
+}
