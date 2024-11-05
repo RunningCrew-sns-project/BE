f9d12d17413b10ca5a4102af97ea2fc897205b73
TaeYoung1217 <wkdxodud44@gmail.com>
2024-11-05 18:53:16 +0900

    feat : 크루원 강퇴, 크루 가입, 거절 로직 구현 중

--- src/main/java/com/github/accountmanagementproject/service/crew/CrewService.java
+++ src/main/java/com/github/accountmanagementproject/service/crew/CrewService.java
@@ -96,7 +96,6 @@ public class CrewService {
     //크루원 퇴장시키기
     @Transactional
     public String sendOutCrew(String crewMasterEmail, Long crewId, Long outUserId) {
-
 //        //내보낼 유저
 //        MyUser outCrewUser = myUsersRepository.findById(outCrewId).orElseThrow(()->new CustomNotFoundException.ExceptionBuilder()
 //                .customMessage("해당 유저를 찾을 수 없습니다.").request(outCrewId).build());
@@ -142,9 +141,10 @@ public class CrewService {
     }
 
     //가입요청을 확인하여 승인, 거절 로직
-    public String approveOrReject(String crewMasterEmail, Long crewId, Integer requestCrewUserId, Boolean approveOrReject) {
+    @Transactional
+    public String approveOrReject(String email, Long crewId, Integer requestCrewUserId, Boolean approveOrReject) {
         //크루 마스터인지 화인
-        if(!crewsRepository.isCrewMaster(crewMasterEmail, crewId)){
+        if(!crewsRepository.isCrewMaster(email, crewId)){
             throw new CustomBadCredentialsException.ExceptionBuilder()
                     .customMessage("크루의 마스터가 아닙니다.")
                     .build();
@@ -164,13 +164,10 @@ public class CrewService {
         if(crewsUsers.getStatus() == CrewsUsersStatus.WAITING && approveOrReject){
             //'가입 완료' 상태로 바꾸고
             crewsUsers.setStatus(CrewsUsersStatus.COMPLETED);
-            //crewsUsers db에 저장
-            crewsUsersRepository.save(crewsUsers);
         }
         else {
-            //TODO : 가입 거절 시 로직 구현
+            //가입 거절 시 '가입 거절' 로 상태 바꾸고
             crewsUsers.setStatus(CrewsUsersStatus.REJECTED);
-            crewsUsersRepository.save(crewsUsers);
         }
 
         return "요청 유저 : " + requestCrewUser + "의 요청을 " + (approveOrReject ? "승인" : "거절") + "했습니다.";
--- src/main/java/com/github/accountmanagementproject/web/controller/blog/BlogControllerDocs.java
+++ src/main/java/com/github/accountmanagementproject/web/controller/blog/BlogControllerDocs.java
@@ -4,6 +4,7 @@ import com.github.accountmanagementproject.web.dto.blog.BlogRequestDTO;
 import com.github.accountmanagementproject.web.dto.responsebuilder.CustomSuccessResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -14,33 +15,24 @@ import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 
-@Tag(name = "Blog", description = "블로그 작성, 좋아요, 댓글 관련 API")
+@Tag(name = "BlogController", description = "블로그 작성, 좋아요, 댓글 관련 API")
 public interface BlogControllerDocs {
 
 
     //블로그 조회
     //TODO : 내가 쓴 글만 보여지게 할지? 필터링 구현 필요할듯
     @Operation(summary = "블로그 조회", description = "모든 블로그 조회, 무한스크롤링 페이지네이션")
-    @ApiResponse(responseCode = "200",description = "블로그 조회 성공",
-            content = @Content(mediaType = "application/json",
-                    examples = {
-                            @ExampleObject(name = "블로그 조회 성공 예",
-                                    description = "블로그 조회에 성공하여 DB에 있는 블로그의 내용이 응답값으로 반환됩니다.",
-                                    value = """
-                                            "  "code": 200," +
-                                            "  "httpStatus": "OK"," +
-                                            "  "message": "블로그 조회 성공"," +
-                                            "  "responseData": "블로그 데이터" +
-                                            """)
-                    })
-    )
-
     @Parameter(name = "size", description = "한번에 보여질 갯수")
     @Parameter(name = "cursor", description = "시작할 블로그 번호")
     @GetMapping
-    public CustomSuccessResponse getAllBlogs(@RequestParam(defaultValue = "10") Integer size,
+    CustomSuccessResponse getAllBlogs(@RequestParam(defaultValue = "10") Integer size,
                                              @RequestParam(required = false) Integer cursor,
                                              @AuthenticationPrincipal String principal);
+
+    @Operation(summary = "블로그 아이디로 조회", description = "블로그 아이디를 받아 블로그 내용 조회")
+    @GetMapping("/{blogId}")
+    CustomSuccessResponse getBlogById(@PathVariable Integer blogId, @AuthenticationPrincipal String principal);
+
 
     @Operation(summary = "블로그 작성", description = "제목, 내용, 기록, 사진을 입력 받아 블로그 작성")
     @ApiResponse(responseCode = "201",description = "블로그 작성 성공",
@@ -79,11 +71,18 @@ public interface BlogControllerDocs {
                     @AuthenticationPrincipal String principal) throws Exception;
 
 
+    @Operation(summary = "블로그 수정", description = "수정할 내용을 받아 블로그 수정")
+    @Parameters({
+            @Parameter(name = "blogId", description = "수정할 블로그 아이디")
+    })
+
     @PutMapping
     CustomSuccessResponse updateBlog(@RequestBody BlogRequestDTO blogRequestDTO,
                                      @RequestParam Integer blogId,
                                      @AuthenticationPrincipal String principal) throws IOException;
 
+    @Operation(summary = "블로그 삭제", description = "블로그 아이디를 받아 블로그 삭제")
+    @Parameter(name = "blogId", description = "삭제할 블로그 아이디")
     @DeleteMapping
     CustomSuccessResponse deleteBlog(@AuthenticationPrincipal String principal,
                                      @RequestParam Integer blogId);
