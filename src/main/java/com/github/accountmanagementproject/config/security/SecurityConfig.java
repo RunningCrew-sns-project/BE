139cfb99efe26b538d397deadae5655fde8d7f93
Sirimp <sihu89@daum.net>
2024-11-14 18:49:16 +0900

    feat: 크루원 리스트 간단 정보 조회

--- src/main/java/com/github/accountmanagementproject/config/security/SecurityConfig.java
+++ src/main/java/com/github/accountmanagementproject/config/security/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
 
     private CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:*"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:*","http://ec2-54-180-9-220.ap-northeast-2.compute.amazonaws.com:8080/"));
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setAllowedMethods(List.of("GET","PUT","POST","PATCH","DELETE","OPTIONS"));
