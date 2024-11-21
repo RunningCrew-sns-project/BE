63856dbb54f24a3cde6768931ff73dcdf08e7442
Sirimp <sihu89@daum.net>
2024-11-21 22:36:10 +0900

    fix : 달리기 버그 수정

--- src/main/java/com/github/accountmanagementproject/alarm/controller/NotificationController.java
+++ src/main/java/com/github/accountmanagementproject/alarm/controller/NotificationController.java
@@ -3,11 +3,11 @@ package com.github.accountmanagementproject.alarm.controller;
 
 import com.github.accountmanagementproject.alarm.sse.SseEmitters;
 import com.github.accountmanagementproject.config.security.AccountConfig;
-import com.github.accountmanagementproject.exception.SimpleRunAppException;
-import com.github.accountmanagementproject.exception.enums.ErrorCode;
 import com.github.accountmanagementproject.repository.account.user.MyUser;
 import com.github.accountmanagementproject.repository.account.user.MyUsersRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,8 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
+import java.util.Optional;
 
-
+@Log4j2
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/notifications")
@@ -39,11 +40,16 @@ public class NotificationController implements NotificationControllerDocs{
     @Override
     public ResponseEntity<SseEmitter> connect(@AuthenticationPrincipal String email) {
 //        MyUser user = accountConfig.findMyUser(email);
-        MyUser admin = userRepository.findByEmail(email)
-                .orElseThrow(() -> new SimpleRunAppException(ErrorCode.USER_NOT_FOUND));
+        Optional<MyUser> admin = userRepository.findByEmail(email);
+        if (admin.isEmpty()){
+            log.warn("해당 사용자를 찾을 수 없습니다.");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+        }
+
+        MyUser user = admin.get();
 
 
-        SseEmitter emitter = sseEmitters.addEmitter(admin.getUserId());
+        SseEmitter emitter = sseEmitters.addEmitter(user.getUserId());
 
         // 이벤트 전송
         try {
@@ -51,7 +57,7 @@ public class NotificationController implements NotificationControllerDocs{
                     .name("connect")
                     .data("Connected successfully"));
         } catch (IOException e) {
-            sseEmitters.removeEmitter(admin.getUserId());
+            sseEmitters.removeEmitter(user.getUserId());
         }
 
         // ResponseEntity를 통해 헤더 설정
